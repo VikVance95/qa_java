63995611cf14d4976cede20e1baec1d5ee7160a3
Artemii Eliseev <vikvance@mail.ru>
2024-12-01 12:55:23 +0300

    Sprint 6 complete

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,33 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+    @Mock
+    Feline feline;
+
+    @Test //Проверка, что метод getSound() возвращает "Мяу"
+    public void getSoundCatTest() {
+        Cat cat = new Cat(feline);
+        String expectedResult = "Мяу";
+        String actualResult = cat.getSound();
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test //Проверка, что метод getFood() возвращает еду для кошки
+    public void getFoodForCatTest() throws Exception {
+        Cat cat = new Cat(feline);
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.eatMeat()).thenReturn(expected);
+        assertEquals(expected, cat.getFood());
+    }
+}
